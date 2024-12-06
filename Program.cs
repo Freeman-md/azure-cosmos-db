a7cc4de42d557f3198903056dea4f4b98bda0810
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-12-06 05:59:10 +0000

    Initial Commit - Setup Cosmos DB Client with Azure Identity

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+
+IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+IConfiguration root = builder.Build();
+
+string? cosmosDBURI = root.GetValue<string>("CosmosDB:URI");
+
+if (string.IsNullOrEmpty(cosmosDBURI))
+{
+    throw new ArgumentNullException(nameof(cosmosDBURI));
+}
+
+CosmosClient client = new CosmosClient(cosmosDBURI, new DefaultAzureCredential());
