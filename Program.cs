f853b460aec96fea60d7c19f26799fdc05e318a2
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-12-06 06:28:34 +0000

    update: implement methods to create database and container

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Azure.Identity;
 using Microsoft.Azure.Cosmos;
 using Newtonsoft.Json;
+using azure_cosmos_db.Services;
 
 IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
 IConfiguration root = builder.Build();
@@ -14,3 +15,24 @@ if (string.IsNullOrEmpty(cosmosDBURI))
 }
 
 CosmosClient client = new CosmosClient(cosmosDBURI, new DefaultAzureCredential());
+
+static async Task CreateDatabaseAndContainer(CosmosClient client, string databaseName, string containerName, string partitionKey) {
+    try
+    {
+        CosmosService cosmosService = new CosmosService(client);
+
+        await cosmosService.CreateDatabase(databaseName);
+
+        await cosmosService.CreateContainer(databaseName, containerName, partitionKey);
+
+        Database database = cosmosService.GetDatabase(databaseName);
+
+        Container container = cosmosService.GetContainer(databaseName, containerName);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+await CreateDatabaseAndContainer(client, "TestDatabase1", "Container1", "/Category");
---
+++ Services/CosmosService.cs
@@ -0,0 +1,44 @@
+using Microsoft.Azure.Cosmos;
+
+namespace azure_cosmos_db.Services;
+
+class CosmosService
+{
+    private readonly CosmosClient _cosmosClient;
+
+    public CosmosService(CosmosClient cosmosClient)
+    {
+        _cosmosClient = cosmosClient;
+    }
+
+    public async Task<DatabaseResponse> CreateDatabase(string databaseName)
+    {
+        return await _cosmosClient.CreateDatabaseIfNotExistsAsync(databaseName);
+    }
+
+    public async Task<ContainerResponse> CreateContainer(string databaseName, string containerName, string partitionKey)
+    {
+        Database database = GetDatabase(databaseName);
+
+        return await database.CreateContainerIfNotExistsAsync(new ContainerProperties()
+        {
+            Id = containerName,
+            PartitionKeyPath = partitionKey
+        });
+    }
+
+    public Database GetDatabase(string databaseName)
+    {
+        return _cosmosClient.GetDatabase(databaseName);
+    }
+
+    public Container GetContainer(string databaseName, string containerName)
+    {
+        Database database = GetDatabase(databaseName);
+
+        return database.GetContainer(containerName);
+    }
+
+
+
+}
